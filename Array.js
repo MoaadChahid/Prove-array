aac624fde457cbe5572a39cb543a020df0b349d3
MoaadChahid <chahidmoaad1@gmail.com>
2026-01-01 18:16:31 +0100

    first commit

---
+++ Array.js
@@ -0,0 +1,33 @@
+
+//ARRAY
+
+
+let Frutta = ["Mela" , "Banana" , "Arancia" , "Pera"];
+
+console.log (`Frutta disponibile: ${Frutta}`);
+
+Frutta.pop();
+
+console.log (`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
+
+Frutta.push ("Kiwi");
+
+
+console.log (`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
+
+Frutta.shift();
+
+Frutta.unshift ("Fragola");
+
+console.log (`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
+
+Frutta.shift();
+
+Frutta.pop();
+
+Frutta.unshift ("Ananas" , "Avocado", "Mandarino" ) ;
+
+
+console.log (`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
+
+console.log (`La lunghezza del gruppo frutta cioè: ${Frutta} e di: ${Frutta.length}` )
