90bc45eb7e4456d96919c30a8052427d7ca8e7a4
MoaadChahid <chahidmoaad1@gmail.com>
2026-01-01 20:01:17 +0100

    Ripasso funzioni con delle operazioni matematiche

--- Array.js
+++ Array.js
@@ -2,32 +2,59 @@
 //ARRAY
 
 
-let Frutta = ["Mela" , "Banana" , "Arancia" , "Pera"];
+let Frutta = ["Mela", "Banana", "Arancia", "Pera"];
 
-console.log (`Frutta disponibile: ${Frutta}`);
-
-Frutta.pop();
-
-console.log (`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
-
-Frutta.push ("Kiwi");
-
-
-console.log (`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
-
-Frutta.shift();
-
-Frutta.unshift ("Fragola");
-
-console.log (`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
-
-Frutta.shift();
+console.log(`Frutta disponibile: ${Frutta}`);
 
 Frutta.pop();
 
-Frutta.unshift ("Ananas" , "Avocado", "Mandarino" ) ;
+console.log(`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
+
+Frutta.push("Kiwi");
 
 
-console.log (`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
+console.log(`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
 
-console.log (`La lunghezza del gruppo frutta cioè: ${Frutta} e di: ${Frutta.length}` )
+Frutta.shift();
+
+Frutta.unshift("Fragola");
+
+console.log(`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
+
+Frutta.shift();
+
+Frutta.pop();
+
+Frutta.unshift("Ananas", "Avocado", "Mandarino");
+
+
+console.log(`Aggiornamento frutta, la frutta disponibile è: ${Frutta}`);
+
+console.log(`La lunghezza del gruppo frutta cioè: ${Frutta} e di: ${Frutta.length}`)
+
+// Funzioni
+
+function somma(a, b) {
+    console.log(a + b);
+}
+
+function meno(a, b) {
+    console.log(a - b);
+
+}
+
+function X(a, b) {
+    console.log(a * b);
+}
+
+function divisione(a, b) {
+    console.log(a / b);
+}
+
+
+
+somma(10, 5);
+meno(10, 5);
+X(10, 5);
+divisione(10, 5)
+
